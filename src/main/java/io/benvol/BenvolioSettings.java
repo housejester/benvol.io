26877a38eb8d07fcbc1a96619b69c1487261194e
Benji Smith <benji@evertrue.com>
2014-03-09 10:27:21 -0400

    started adding index-awareness to the BenvolioSettings class

--- src/main/java/io/benvol/BenvolioSettings.java
+++ src/main/java/io/benvol/BenvolioSettings.java
@@ -12,20 +12,27 @@ import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
 public class BenvolioSettings {
-    
+
     private final String _environment;
     private final int _serverPort;
     private final int _threadPoolSize;
-    
+
     private final List<KeyValuePair<String, Integer>> _elasticHosts;
 
+    private final List<String> _indexNames;
+
+    private final String _userTypeName;
+    private final String _sessionTypeName;
+    private final String _groupTypeName;
+    private final String _roleTypeName;
+
     public BenvolioSettings(ObjectNode json) {
-        
+
         // Basic top-level settings
         _environment = json.get("environment").asText();
         _serverPort = json.get("server").get("port").asInt();
         _threadPoolSize = json.get("thread_pool_size").asInt();
-        
+
         // Elastic hostnames and ports
         List<KeyValuePair<String, Integer>> elasticHosts = Lists.newArrayList();
         for (JsonNode node : ((ArrayNode) json.get("elastic").get("hosts"))) {
@@ -34,12 +41,25 @@ public class BenvolioSettings {
             elasticHosts.add(new KeyValuePair<>(hostname, restPort));
         }
         _elasticHosts = Collections.unmodifiableList(elasticHosts);
+
+        // Elastic index names
+        List<String> indexNames = Lists.newArrayList();
+        for (JsonNode node : ((ArrayNode) json.get("elastic").get("index_names"))) {
+            indexNames.add(node.asText());
+        }
+        _indexNames = Collections.unmodifiableList(indexNames);
+
+        // Well-known types in the authentication system
+        _userTypeName = json.get("elastic").get("auth").get("user").get("type_name").asText();
+        _sessionTypeName = json.get("elastic").get("auth").get("session").get("type_name").asText();
+        _groupTypeName = json.get("elastic").get("auth").get("group").get("type_name").asText();
+        _roleTypeName = json.get("elastic").get("auth").get("role").get("type_name").asText();
     }
-    
+
     public String getEnvironment() {
         return _environment;
     }
-    
+
     public int getThreadPoolSize() {
         return _threadPoolSize;
     }
@@ -47,9 +67,29 @@ public class BenvolioSettings {
     public int getServerPort() {
         return _serverPort;
     }
-    
+
     public List<KeyValuePair<String, Integer>> getElasticHosts() {
         return _elasticHosts;
     }
-    
+
+    public List<String> getIndexNames() {
+        return _indexNames;
+    }
+
+    public String getUserTypeName() {
+        return _userTypeName;
+    }
+
+    public String getSessionTypeName() {
+        return _sessionTypeName;
+    }
+
+    public String getGroupTypeName() {
+        return _groupTypeName;
+    }
+
+    public String getRoleTypeName() {
+        return _roleTypeName;
+    }
+
 }

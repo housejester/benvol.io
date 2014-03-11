6600c78f0dfd3baeebb94ed231761f34f0c07236
Benji Smith <benji@evertrue.com>
2014-03-11 01:17:52 -0400

    added fully-qualified-name parsing to JsonUtil

--- src/main/java/io/benvol/util/JsonUtil.java
+++ src/main/java/io/benvol/util/JsonUtil.java
@@ -9,15 +9,41 @@ import com.google.common.collect.Lists;
 
 public class JsonUtil {
 
+    public static JsonNode findJsonNodeByFullyQualifiedName(JsonNode json, String name) {
+        JsonNode currentNode = json;
+        for (String nameSegment : name.split("\\.")) {
+            if (currentNode.has(nameSegment)) {
+                currentNode = currentNode.get(nameSegment);
+            } else {
+                return null;
+            }
+        }
+        return currentNode;
+    }
+
+    public static String getStringForKey(JsonNode json, String key) {
+        JsonNode node = findJsonNodeByFullyQualifiedName(json, key);
+        if (node == null) {
+            return null;
+        } else if (node.isTextual()) {
+            return node.asText();
+        } else {
+            throw new RuntimeException(String.format(
+                "can't get string for key '%s' from json: %s",
+                key, json.toString()
+            ));
+        }
+    }
+
     public static List<String> getStringListForKey(JsonNode json, String key) {
         List<String> list = Collections.emptyList();
-        if (json.has(key)) {
-            JsonNode value = json.get(key);
-            if (value.isTextual()) {
-                list = Collections.singletonList(value.asText());
-            } else if (value.isArray()) {
-                list = Lists.newArrayListWithExpectedSize(value.size());
-                for (JsonNode child : (ArrayNode) value) {
+        JsonNode node = findJsonNodeByFullyQualifiedName(json, key);
+        if (node != null) {
+            if (node.isTextual()) {
+                list = Collections.singletonList(node.asText());
+            } else if (node.isArray()) {
+                list = Lists.newArrayListWithExpectedSize(node.size());
+                for (JsonNode child : (ArrayNode) node) {
                     list.add(child.asText());
                 }
             } else {

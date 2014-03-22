498e1ba206a7a1c1f50d3a80794da8cbf50522cd
Benji Smith <benji@evertrue.com>
2014-03-22 11:30:23 -0400

    the identify predicate should always include the name of the 'user' type

--- src/main/java/io/benvol/model/auth/IdentifyPredicate.java
+++ src/main/java/io/benvol/model/auth/IdentifyPredicate.java
@@ -6,33 +6,24 @@ import java.util.Map;
 import com.google.common.collect.Lists;
 
 public class IdentifyPredicate {
-    
-    private static final String HEADER_PREFIX = "auth.identify:";
-    
-    private final boolean _userTypeImplicit;
+
+    private static final String HEADER_PREFIX = "auth.identify.";
+
     private final String _userType;
     private final String _qualifiedField;
     private final String _operand;
-    
+
     public IdentifyPredicate(String userType, String qualifiedField, String operand) {
-        _userTypeImplicit = userType == null;
         _userType = userType;
         _qualifiedField = qualifiedField;
         _operand = operand;
     }
-    
+
     public IdentifyPredicate(String headerName, String headerValue) {
         String field = headerName.substring(HEADER_PREFIX.length() + 1);
         int dotPosition = field.indexOf('.');
-        if (dotPosition >= 0) {
-            _userTypeImplicit = false;
-            _userType = field.substring(0, dotPosition);
-            _qualifiedField = field.substring(dotPosition + 1);
-        } else {
-            _userTypeImplicit = true;
-            _userType = null;
-            _qualifiedField = field;
-        }
+        _userType = field.substring(0, dotPosition);
+        _qualifiedField = field.substring(dotPosition + 1);
         _operand = headerValue;
     }
 
@@ -47,21 +38,17 @@ public class IdentifyPredicate {
         }
         return predicates;
     }
-    
-    public boolean isUserTypeImplicit() {
-        return _userTypeImplicit;
-    }
-    
+
     public String getUserType() {
         return _userType;
     }
-    
+
     public String getQualifiedField() {
         return _qualifiedField;
     }
-    
+
     public String getOperand() {
         return _operand;
     }
-    
+
 }

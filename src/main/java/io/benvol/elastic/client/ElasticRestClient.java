7ef464d2d2db6183cb12357e1b68b5d73d6fb415
Benji Smith <benji@evertrue.com>
2014-03-24 21:58:18 -0400

    wrote some notes about two-phase policy resolution

--- src/main/java/io/benvol/elastic/client/ElasticRestClient.java
+++ src/main/java/io/benvol/elastic/client/ElasticRestClient.java
@@ -254,6 +254,12 @@ public class ElasticRestClient {
     }
 
     public List<Policy> findPoliciesFor(AuthUser authUser, ElasticHttpRequest elasticHttpRequest) {
+        // TODO: Perform an elasticsearch query to find candidate policies for this user and
+        // for this request. Then, eliminate candidate policies that don't specifically apply
+        // to this request. This two-phase candidate-elimination process is necessary because
+        // policies contain certain types of predicates that can't be matched via any kind of
+        // elasticsearch query. (For example, using a regex in an indexed policy to match
+        // against a string literal in a user query.)
         throw new RuntimeException("NOT IMPLEMENTED"); // TODO
     }
 }
